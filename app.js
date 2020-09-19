c96c68ffdd24f63341250a8741480817332c63b2
Soma Kiran Kumar Nellipudi <snellipudi@InComm.com>
2020-09-19 17:15:57 -0400

    deleting views

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+/*// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'pug');*/
 
 app.use(logger('dev'));
 app.use(express.json());
